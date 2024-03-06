535dc5824f8c13be7303796aa76d944befd61a6c
Oliver A Wang <oliveraw@gl-login3.arc-ts.umich.edu>
2024-03-06 12:14:00 -0500

    training resnet

--- run.py
+++ run.py
@@ -3,15 +3,23 @@ import logging
 import random
 import torch
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
 from mmocr.apis import MMOCRInferencer
 from torchvision.io import ImageReadMode
+from torchvision import transforms
+from torchvision.models import resnet50, ResNet50_Weights
+from torchsummary import summary
+from torch.utils.data import DataLoader
 
-from dataset import soccernet_dataset
+from soccernet_dataset import soccernet_dataset, soccernet_dataset_flat, generate_all_file_names
+from resnet import ResNet18, ResNet50
 
 parser = argparse.ArgumentParser(description='EECS 545 SoccerNet Jersey Number Recognition')
 parser.add_argument('--batch_size', default=64, type=int, help='train batchsize') 
 parser.add_argument('--lr', '--learning_rate', default=0.001, type=float, help='initial learning rate')
 parser.add_argument('--num_epochs', default=300, type=int)
+parser.add_argument('--num_epochs_per_eval', default=100, type=int)
 parser.add_argument('--seed', default=123)
 parser.add_argument('--det_threshold', default=0.3, type=float)
 parser.add_argument('--data_path', default='./data', type=str, help='path to dataset, the dir with (train, test, challenge) directories')
@@ -31,36 +39,84 @@ random.seed(args.seed)
 torch.manual_seed(args.seed)
 torch.cuda.manual_seed_all(args.seed)
 
-train_dataset = soccernet_dataset(args.data_path, "train")
-test_dataset = soccernet_dataset(args.data_path, "test")
-logger.info(f"Num videos in train dataset: {len(train_dataset)}")
-logger.info(f"Num videos in test dataset: {len(test_dataset)}")
+# model
+transforms = transforms.Compose([
+    transforms.Resize((128, 64)),
+    transforms.Normalize((127.4395, 135.9471,  84.0932), (38.5333, 38.7357, 47.5903))
+])
 
-infer = MMOCRInferencer(det='dbnetpp', device=device)
+model = ResNet50(num_classes=101).to(device)
+summary(model, (3, 128, 64))
 
-for (frames, frame_paths, gt) in train_dataset:
-    # output of inferencer is in this format: https://mmocr.readthedocs.io/en/dev-1.x/user_guides/inference.html#output
-    result = infer(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
-    for idx, pred in enumerate(result['predictions']):
-        det_scores = pred['det_scores']
-        over_threshold = len(det_scores) != 0 and any(i >= args.det_threshold for i in det_scores)
-        print(det_scores)
-        if over_threshold:
-            filename = frame_paths[idx].split('/')[-1]
-            plt.figure()
-            plt.title(det_scores)
-            plt.imshow(result['visualization'][0])
-            plt.savefig(f"{args.output_dir}/{filename}")
-            logger.debug(f"Saving figure {filename}")
+# data
+train_dataset = soccernet_dataset_flat(args.data_path, "train", transforms)
+test_dataset = soccernet_dataset_flat(args.data_path, "test", transforms)
+logger.info(f"Num frames in train dataset: {len(train_dataset)}")
+logger.info(f"Num frames in test dataset: {len(test_dataset)}")
 
-    # for path in frame_paths:
-    #     # output of inferencer is in this format: https://mmocr.readthedocs.io/en/dev-1.x/user_guides/inference.html#output
-    #     result = infer(path, out_dir=args.output_dir, save_vis=False, return_vis=True)
-    #     det_scores = result['predictions'][0]['det_scores']
-    #     over_threshold = len(det_scores) != 0 and all(i >= args.det_threshold for i in det_scores)
-    #     print(result['predictions'][0]['det_scores'])
-    #     if over_threshold: 
-    #         plt.figure()
-    #         plt.imshow(result['visualization'][0])
-    #         plt.savefig(path)
-    #         logging.debug(f"Saving figure {path}")
+train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+# training 
+criterion = nn.CrossEntropyLoss()
+optim = optim.Adam(model.parameters(), lr=args.lr)
+for epoch in range(args.num_epochs+1):
+    running_loss = 0.0
+    for i, data in enumerate(train_dataloader):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optim.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optim.step()
+
+        running_loss += loss.item()
+    logger.info(f'Epoch {epoch} loss: {running_loss}')
+
+    if epoch % args.num_epochs_per_eval == 0:
+        model.eval()
+        with torch.no_grad():
+            running_loss = 0.0
+            for i, data in enumerate(test_dataloader):
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
+            logger.info(f'Epoch {epoch} test loss: {running_loss}')
+        model.train()
+
+# for i in range(10):
+#     frame, gt = train_dataset[i]
+#     frame = frame.permute(1, 2, 0)
+#     plt.figure()
+#     plt.imshow(frame)
+#     plt.savefig(f'outputs/{i}.jpg')
+
+
+
+
+##########################non-flat###############################
+# train_dataset = soccernet_dataset(args.data_path, "train")
+# test_dataset = soccernet_dataset(args.data_path, "test")
+# logger.info(f"Num videos in train dataset: {len(train_dataset)}")
+# logger.info(f"Num videos in test dataset: {len(test_dataset)}")
+
+# infer = MMOCRInferencer(rec='dbnetpp', device=device)
+
+# for (frames, frame_paths, gt) in train_dataset:
+#     # output of inferencer is in this format: https://mmocr.readthedocs.io/en/dev-1.x/user_guides/inference.html#output
+#     result = infer(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
+#     for idx, pred in enumerate(result['predictions']):
+#         det_scores = pred['det_scores']
+#         over_threshold = len(det_scores) != 0 and any(i >= args.det_threshold for i in det_scores)
+#         print(det_scores, rec_texts)
+#         if over_threshold:
+#             filename = frame_paths[idx].split('/')[-1]
+#             plt.figure()
+#             plt.title(det_scores)
+#             plt.imshow(result['visualization'][0])
+#             plt.savefig(f"{args.output_dir}/{filename}")
+#             logger.debug(f"Saving figure {filename}")
---
+++ soccernet_dataset.py
@@ -0,0 +1,108 @@
+import os
+import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+
+# the index gives the video--e.g. idx 1 = video 1 (num_frames x H x W x 3)
+class soccernet_dataset(Dataset):
+    def __init__(self, root_dir, mode):
+        self.vid_dir = os.path.join(root_dir, mode, "images")           # data/train/images
+        vid_names = [f for f in os.listdir(self.vid_dir) if not f.startswith(".")]
+        self.vid_names = sorted(vid_names, key=lambda x: int(x))        # [0, 1, 2, ...]
+
+        gt_file = os.path.join(root_dir, mode, f"{mode}_gt.json")
+        with open(gt_file) as f:
+            self.gt = json.load(f)
+
+        assert len(self.vid_names) == len(self.gt)
+
+    def __len__(self):
+        return len(self.vid_names)
+
+    # each video is a list of frames, each frame is shape (3, H, W)
+    def __getitem__(self, idx):
+        vid_name = self.vid_names[idx]
+        vid_dir = os.path.join(self.vid_dir, vid_name)
+
+        frame_names = [f for f in os.listdir(vid_dir) if not f.startswith(".")]
+        frame_names = sorted(frame_names, key=lambda x: int(x.split('.')[0].split('_')[1]))
+        frame_paths = [os.path.join(vid_dir, frame_name) for frame_name in frame_names]
+        frames = []
+        for frame_path in frame_paths:
+            frame = read_image(frame_path)
+            frames.append(frame)
+        return frames, frame_paths, self.gt[vid_name]
+
+
+# images will not be in order
+# get item will just return a single frame rather than a whole video
+class soccernet_dataset_flat(Dataset):
+    def __init__(self, root_dir, mode, transform=None):
+        # if you pre-generated all the file names (much faster)
+        frames_file = os.path.join(root_dir, mode, "frames.json")
+        with open(frames_file) as f:
+            frames_dict = json.load(f)
+
+        gt_file = os.path.join(root_dir, mode, f"{mode}_gt.json")
+        with open(gt_file) as f:
+            gt_dict = json.load(f)
+        gt_vals = gt_dict.values()
+        # print("ground truth possible values", mode, sorted(gt_vals, key=lambda x: int(x)))
+        
+        self.frame_paths = []
+        self.gt = []
+        for vid_name, frame_list in frames_dict.items():
+            frame_paths = [os.path.join(root_dir, mode, "images", vid_name, f) for f in frame_list]
+            self.frame_paths.extend(frame_paths)
+            self.gt.extend([gt_dict[vid_name]] * len(frame_paths))
+
+        # otherwise, need to get all the files
+        # data_dir = os.path.join(root_dir, mode, "images")           # data/train/images
+        # vid_names = [f for f in os.listdir(data_dir) if not f.startswith(".")]
+        # vid_names = sorted(vid_names, key=lambda x: int(x))        # [0, 1, 2, ...]
+        # self.frame_paths = []
+        # self.gt = []
+        # for vid_name in vid_names:
+        #     vid_dir = os.path.join(data_dir, vid_name)
+        #     frame_names = [f for f in os.listdir(vid_dir) if not f.startswith(".")]
+        #     frame_names = sorted(frame_names, key=lambda x: int(x.split('.')[0].split('_')[1]))
+        #     frame_paths_for_this_vid = [os.path.join(vid_dir, f) for f in frame_names]
+        #     self.frame_paths.extend(frame_paths_for_this_vid)
+        #     self.gt.extend([gt_dict[vid_name]] * len(frame_paths_for_this_vid))
+
+        assert len(self.frame_paths) == len(self.gt)
+
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.frame_paths)
+
+    def __getitem__(self, idx):
+        frame = read_image(self.frame_paths[idx]).float()
+
+        if self.transform:
+            frame = self.transform(frame)
+        
+        if self.gt[idx] == -1:
+            return frame, 100
+        else:
+            return frame, self.gt[idx]
+
+
+# generate_all_file_names('./data/train/images', './data/train/frames.txt')
+# generate_all_file_names('./data/test/images', './data/test/frames.txt')
+# generate_all_file_names('./data/challenge/images', './data/challenge/frames.txt')
+def generate_all_file_names(data_dir, output_file):
+    frame_paths_per_vid = {}
+    vid_names = [f for f in os.listdir(data_dir) if not f.startswith(".")]
+    vid_names = sorted(vid_names, key=lambda x: int(x))
+    for vid_name in vid_names:
+        print(vid_name)
+        vid_dir = os.path.join(data_dir, vid_name)
+        frame_names = [f for f in os.listdir(vid_dir) if not f.startswith(".")]
+        frame_names = sorted(frame_names, key=lambda x: int(x.split('.')[0].split('_')[1]))
+        frame_paths_per_vid[vid_name] = frame_names
+    
+    with open(output_file, 'w') as f:
+        json.dump(frame_paths_per_vid, f)
