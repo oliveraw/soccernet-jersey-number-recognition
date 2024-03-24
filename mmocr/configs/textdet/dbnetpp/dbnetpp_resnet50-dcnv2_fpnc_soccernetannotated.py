1855c84b919b2e60e6e12222d0a59f2f2195bdba
Oliver A Wang <oliveraw@gl-login3.arc-ts.umich.edu>
2024-03-24 18:08:39 -0400

    added mmocr

---
+++ mmocr/configs/textdet/dbnetpp/dbnetpp_resnet50-dcnv2_fpnc_soccernetannotated.py
@@ -0,0 +1,121 @@
+_base_ = [
+    '_base_dbnetpp_resnet50-dcnv2_fpnc.py',         # model architecture
+    '../_base_/datasets/soccernet.py',              # get our custom dataset
+    '../_base_/default_runtime.py',                 # default configurations for eval, logging  
+    '../_base_/schedules/schedule_adam_10e.py',     # get the optimizer + num epochs
+]
+
+load_from = 'https://download.openmmlab.com/mmocr/textdet/dbnetpp/dbnetpp_resnet50-dcnv2_fpnc_1200e_icdar2015/dbnetpp_resnet50-dcnv2_fpnc_1200e_icdar2015_20220829_230108-f289bd20.pth'
+
+# dataset settings
+train_list = [_base_.soccernet_textdet_train]
+val_list = [_base_.soccernet_textdet_val]
+test_list = [_base_.soccernet_textdet_test]
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    dict(
+        type='LoadOCRAnnotations',
+        with_bbox=True,
+        with_polygon=True,
+        with_label=True,
+    ),
+    dict(
+        type='TorchVisionWrapper',
+        op='ColorJitter',
+        brightness=32.0 / 255,
+        saturation=0.5),
+    dict(
+        type='ImgAugWrapper',
+        args=[['Fliplr', 0.5],
+              dict(cls='Affine', rotate=[-10, 10]), ['Resize', [0.5, 3.0]]]),
+    dict(type='RandomCrop', min_side_ratio=0.4),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='Pad', size=(640, 640)),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape'))
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='Pad', size=(640, 640)),
+    dict(
+        type='LoadOCRAnnotations',
+        with_polygon=True,
+        with_bbox=True,
+        with_label=True,
+    ),
+    dict(
+        type='PackTextDetInputs',
+        meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor',
+                   'instances'))
+]
+# test_pipeline = [
+#     dict(type='LoadImageFromFile', color_type='color_ignore_orientation'),
+#     dict(
+#         type='LoadOCRAnnotations',
+#         with_bbox=True,
+#         with_polygon=True,
+#         with_label=True,
+#     ),
+#     dict(
+#         type='TorchVisionWrapper',
+#         op='ColorJitter',
+#         brightness=32.0 / 255,
+#         saturation=0.5),
+#     dict(
+#         type='ImgAugWrapper',
+#         args=[['Fliplr', 0.5],
+#               dict(cls='Affine', rotate=[-10, 10]), ['Resize', [0.5, 3.0]]]),
+#     dict(type='RandomCrop', min_side_ratio=0.4),
+#     dict(type='Resize', scale=(640, 640), keep_ratio=True),
+#     dict(type='Pad', size=(640, 640)),
+#     dict(
+#         type='PackTextDetInputs',
+#         meta_keys=('img_path', 'ori_shape', 'img_shape', 'scale_factor', 'instances'))
+# ]
+
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type='ConcatDataset',
+        datasets=train_list,
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='ConcatDataset',
+        datasets=val_list,
+        pipeline=test_pipeline))
+
+test_dataloader = dict(
+    batch_size=8,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='ConcatDataset',
+        datasets=test_list,
+        pipeline=test_pipeline))
+
+auto_scale_lr = dict(base_batch_size=8)
+
+_base_.model.data_preprocessor=dict(
+    type='TextDetDataPreprocessor',
+    mean=[127.4395, 135.9471,  84.0932],
+    std=[38.5333, 38.7357, 47.5903],
+    bgr_to_rgb=True,
+    pad_size_divisor=32)
+
+# visualization = _base_.default_hooks.visualization
+# visualization.update(
+#     dict(enable=True, show=True, draw_gt=False, draw_pred=True))
