ea42eebc542891efa11396c37b6cef4c9f7b469b
Oliver A Wang <oliveraw@gl-login3.arc-ts.umich.edu>
2024-03-24 18:08:39 -0400

    added mmocr

---
+++ mmocr/configs/textdet/_base_/datasets/soccernet.py
@@ -0,0 +1,22 @@
+# https://mmocr.readthedocs.io/en/dev-1.x/basic_concepts/transforms.html
+
+soccernet_textdet_train = dict(
+    type='OCRDataset',
+    data_root='data/soccernet-annotated/train',
+    ann_file='_mmocr_annotations.json',
+    filter_cfg=dict(filter_empty_gt=True, min_size=32),
+    pipeline=None)
+
+soccernet_textdet_val = dict(
+    type='OCRDataset',
+    data_root='data/soccernet-annotated/valid',
+    ann_file='_mmocr_annotations.json',
+    filter_cfg=dict(filter_empty_gt=True, min_size=32),
+    pipeline=None)
+
+soccernet_textdet_test = dict(
+    type='OCRDataset',
+    data_root='data/soccernet-annotated/test',
+    ann_file='_mmocr_annotations.json',
+    filter_cfg=dict(filter_empty_gt=True, min_size=32),
+    pipeline=None)
