63df356164097ea0e20b6237807d2ea1582df066
Oliver A Wang <oliveraw@gl-login1.arc-ts.umich.edu>
2024-03-24 21:40:25 -0400

    run through batch script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,13 +6,13 @@
 #SBATCH --job-name=soccernet
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
 #SBATCH --ntasks-per-node=2
-#SBATCH --mem-per-cpu=8000m 
 #SBATCH --time=7:59:55
 #SBATCH --account=eecs545w24_class
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
+#SBATCH --mem-per-gpu=16000m 
+#SBATCH --cpus-per-gpu=3
 #SBATCH --output=./logs/%x-%j.log
 
 # The application(s) to execute along with its input arguments and options:
