231f4c2b211387f301d45e7203e065f2a4198dda
Oliver A Wang <oliveraw@gl-login1.arc-ts.umich.edu>
2024-04-11 14:56:44 -0400

    updated run.py

--- run.py
+++ run.py
@@ -11,20 +11,28 @@ import cv2
 import scipy
 import numpy as np
 import os
+import json
 
-from soccernet_dataset import soccernet_dataset, soccernet_dataset_flat, generate_all_file_names
+from soccernet_dataset import soccernet_dataset, soccernet_dataset_flat
 
 parser = argparse.ArgumentParser(description='EECS 545 SoccerNet Jersey Number Recognition')
 parser.add_argument('--seed', default=123)
 parser.add_argument('--det_threshold', default=0.6, type=float)
+parser.add_argument('--rec_threshold', default=0.9, type=float)
 parser.add_argument('--data_path', default='./data', type=str, help='path to dataset, the dir with (train, test, challenge) directories')
 parser.add_argument('--output_dir', default='./outputs', type=str, help='directory to store outputs')
-parser.add_argument('--det_config_path', default='mmocr/configs/textdet/dbnetpp/dbnetpp_resnet50-dcnv2_fpnc_soccernetannotated.py', type=str, help='python file which defines architecture and training configurations')
-parser.add_argument('--det_weights_path', default='mmocr/soccernet-dbnetpp/epoch_10.pth', type=str, help='weights for the finetuned detector')
+parser.add_argument('--det_config_path', default='mmocr/configs/textdet/dbnetpp/dbnetpp_resnet50-dcnv2_fpnc_soccernetannotated_gen.py', type=str, help='python file which defines architecture and training configurations')
+parser.add_argument('--det_weights_path', default='mmocr/soccernet-dbnetpp/epoch_30.pth', type=str, help='weights for the finetuned detector')
+parser.add_argument('--rec_config_path', default='mmocr/configs/textrecog/svtr/svtr-base_20e_soccernet_gen.py', type=str, help='python file which defines architecture and training configurations')
+parser.add_argument('--rec_weights_path', default='mmocr/soccernet-svtr/epoch_20.pth', type=str, help='weights for the finetuned recognizer')
+parser.add_argument('--save_vis', action='store_true')
 args = parser.parse_args()
 
+# create the output and vis directories
+os.makedirs(f"{args.output_dir}/soccernet-{os.getenv('SLURM_JOB_ID')}/vis", exist_ok=True)
+
 # toggle between INFO, DEBUG
-logfile = f"logs/soccernet-{os.getenv('SLURM_JOB_ID')}-info.log"
+logfile = f"{args.output_dir}/soccernet-{os.getenv('SLURM_JOB_ID')}/output.log"
 logging.basicConfig(filename=logfile,
     format='%(asctime)s %(message)s', 
     level=logging.DEBUG)
@@ -43,44 +51,78 @@ test_dataset = soccernet_dataset(args.data_path, "test")
 logger.info(f"Num videos in train dataset: {len(train_dataset)}")
 logger.info(f"Num videos in test dataset: {len(test_dataset)}")
 
-infer = MMOCRInferencer(det=args.det_config_path, det_weights=args.det_weights_path, rec='SAR', device=device)
+# det = MMOCRInferencer(det=args.det_config_path,
+#     det_weights=args.det_weights_path,
+#     device=device)
+# rec = MMOCRInferencer(rec=args.rec_config_path,
+#     rec_weights=args.rec_weights_path,
+#     device=device)
+
+infer = MMOCRInferencer(det=args.det_config_path,
+    det_weights=args.det_weights_path,
+    rec=args.rec_config_path,
+    rec_weights=args.rec_weights_path,
+    device=device)
 
 correct = []
-for video_idx, (frame_paths, gt) in enumerate(train_dataset):
+for video_idx, (frame_paths, gt) in enumerate(test_dataset):
     # output of inferencer is in this format: https://mmocr.readthedocs.io/en/dev-1.x/user_guides/inference.html#output
 
     # for debugging to skip most frames
-    # frame_paths = [frame_paths[0]]
+    # frame_paths = frame_paths[:88]
 
-    result = infer(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
-    predictions = []
-    for idx, pred in enumerate(result['predictions']):
-        det_scores = pred['det_scores']
-        rec_scores = pred['rec_scores']
-        rec_texts = pred['rec_texts']
-        # print(idx, det_scores, rec_texts)
-
-        # filter non numeric predictions, take only predictions with det confidence above threshold
-        for i, score in enumerate(det_scores):
-            text = rec_texts[i]
-            if score > args.det_threshold and text.isnumeric():
-                predictions.append(int(text))
-
-        # save the images which were over the detection threshold
-        # over_threshold = len(det_scores) != 0 and any(i >= args.det_threshold for i in det_scores)
-        # if over_threshold:
-        #     filename = frame_paths[idx].split('/')[-1]
-        #     plt.figure()
-        #     plt.title(det_scores)
-        #     plt.imshow(result['visualization'][0])
-        #     plt.savefig(f"{args.output_dir}/{filename}")
-        #     logger.debug(f"Saving figure {filename}")
-
-    predictions = np.array(predictions)
-    final_prediction = scipy.stats.mode(predictions, axis=None, keepdims=False)[0]
-    if np.isnan(final_prediction):
+    # FIX THIS - predicts non soccerball as soccerball sometimes
+    max_shape = 0
+    for path in frame_paths:
+        img = cv2.imread(path)
+        max_shape = max(max_shape, img.shape[0])
+        max_shape = max(max_shape, img.shape[1])
+    
+    if max_shape < 50:
         final_prediction = -1
-    logger.info(f"Video: {video_idx}, Prediction: {final_prediction}, Ground truth: {gt} Correct?: {final_prediction == gt}, {predictions}")
+        logger.info(f"Video: {video_idx}, soccer ball shortcut prediction")
+    else:
+        # det_result = det(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
+        # rec_result = rec(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
+        # print(det_result['predictions'][0])
+        # assert False
+
+        result = infer(frame_paths, out_dir=args.output_dir, save_vis=False, return_vis=True)
+
+        predictions = []
+        for idx, pred in enumerate(result['predictions']):
+            det_scores = pred['det_scores']
+            rec_scores = pred['rec_scores']
+            rec_texts = pred['rec_texts']
+            # print(idx, det_scores, rec_texts)
+            predictions.extend(list(zip(det_scores, rec_scores, rec_texts)))
+
+            # save the images which were over the detection threshold
+            if args.save_vis:
+                over_threshold = len(det_scores) != 0 and any(i >= args.det_threshold for i in det_scores)
+                if over_threshold:
+                    filename = frame_paths[idx].split('/')[-1]
+                    plt.figure()
+                    plt.title(det_scores)
+                    plt.imshow(result['visualization'][0])
+                    plt.savefig(f"{args.output_dir}/soccernet-{os.getenv('SLURM_JOB_ID')}/vis/{filename}")
+                    logger.debug(f"Saving figure {filename}")
+
+        confident_numbers = []
+        # filter non numeric predictions, take only predictions with det confidence above threshold
+        for i, (det_score, rec_score, rec_text) in enumerate(predictions):
+            if det_score > args.det_threshold and rec_score > args.rec_threshold and rec_text.isnumeric():
+                confident_numbers.append(int(rec_text))
+
+        confident_numbers = np.array(confident_numbers)
+        final_prediction = scipy.stats.mode(confident_numbers, axis=None, keepdims=False)[0]
+        if np.isnan(final_prediction):
+            final_prediction = -1
+
     correct.append(final_prediction == gt)
 
-logger.info(f"Final Accuracy: {count(correct)}/{len(correct)} = {count(correct) / len(correct)}")
+    logger.info(f"Video: {video_idx}, Prediction: {final_prediction}, Ground truth: {gt} Correct?: {final_prediction == gt}")
+    for pred in predictions:
+        logger.debug(pred)
+
+logger.info(f"Final Accuracy: {sum(correct)}/{len(correct)} = {sum(correct) / len(correct)}")
